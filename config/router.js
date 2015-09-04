68bd28c1d0a50fe3d12221fb052f248602108ca0
anjmendoza <memendoza3@up.edu.ph>
2015-09-04 12:39:32 +0800

    september 04

--- config/router.js
+++ config/router.js
@@ -2,10 +2,14 @@ var student = require('./../controllers/student');
 var teacher = require('./../controllers/teacher');
 //function for route handling
 module.exports=function(router){
-	router.route('/student')
-		.get(student.find);
-	
-	router.route('/teacher')
+	router.route('/students')
+		.get(student.find)
+		.post(student.insert);
+	router.route('/students/:id')
+		.get(student.find1)
+		//.put(student.update);
+		.delete(student.remove);
+	router.route('/teachers')
 		.get(teacher.get)
 		.put(teacher.put)
 		.post(teacher.post)

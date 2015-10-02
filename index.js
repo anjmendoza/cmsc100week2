d8d74bbd80eaa50e9cd5e34561468d2f2bbfcfc5
anjmendoza <memendoza3@up.edu.ph>
2015-10-02 11:34:31 +0800

    HTML

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ var server = app.listen(5000, function(){
 app.use(require('body-parser')()); //we can get the data na binigay ng client using get.body
 app.use(require('method-override')());
 app.use(require(__dirname + '/config/router')(express.Router()));
+app.use(express.static(__dirname + '/public'));
 
 	
